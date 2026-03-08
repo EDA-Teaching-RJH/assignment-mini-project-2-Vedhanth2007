293acf0aebdedfbace9b63de5393d32246dd7604
Vedhanth2007 <vs396@kent.ac.uk>
2026-03-08 13:05:57 +0000

    added the ability to read and write to the logs

--- log_parser.py
+++ log_parser.py
@@ -1,10 +1,18 @@
 import re
 
-def check_ip():
-    pattern = ("r^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$")
-    inp = input("Enter ip")
-    p4 = re.compile(pattern)
-    if re.search(p4,inp):
-        print("Valid IP")
-    else:
-        print("Invalid IP")
+#def check_ip():
+    #pattern = 
+    #inp = input("Enter ip")
+    #p4 = re.compile(pattern)
+    #if re.search(p4,inp):
+        #print("Valid IP")
+    #else:
+        #print("Invalid IP")
+
+log_file = "server.log.txt"
+
+with open(log_file, "a") as f:
+  f.write("Now the file has more content!")
+
+with open(log_file) as f:
+  print(f.read())
