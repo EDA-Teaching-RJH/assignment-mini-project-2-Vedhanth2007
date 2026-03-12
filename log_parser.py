5ab5471365b2dc8b35bb805382aa1b27d9984601
Vedhanth2007 <vs396@kent.ac.uk>
2026-03-12 10:01:59 +0000

    made log analyze, still needs bug fix. It is used to print out the logs that is not debugs

--- log_parser.py
+++ log_parser.py
@@ -1,24 +1,43 @@
 import re
 
-pattern = 
+pattern = r'\[(\w+)\] (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}): (.*)'
 
-log_file = "server.log.txt"
+log_file = ["[DEBUG] 2023-05-25 10:15:32: Initializing application...",
+    "[INFO] 2023-05-25 10:15:35: User 'John' logged in.",
+    "[ERROR] 2023-05-25 10:15:40: Database connection failed.",
+    "[DEBUG] 2023-05-25 10:15:45: Processing request...",
+    "[INFO] 2023-05-25 10:15:50: Request completed successfully."]
 
-with open(log_file, "a") as f:
-  f.write("Now the file has more content!")
+#with open(log_file, "a") as f:
+  #f.write("Now the file has more content!")
 
-with open(log_file) as f:
-  print(f.read())
+#with open(log_file) as f:
+  #print(f.read())
 
-class log_entry:
-  def __init__(self,timestamp,msg):
-    self.timestamp = timestamp
-    self.msg = msg
+#class log_entry:
+  #def __init__(self,timestamp,msg):
+    #self.timestamp = timestamp
+    #self.msg = msg
 
-  def __repr__(self):
-    return f"{self.timestamp}, {self.msg}"
+  #def __repr__(self):
+    #return f"{self.timestamp}, {self.msg}"
 
 class analyze_logs:
-  checked_logs = []
-  for log in #####:
-    match = re.match()
+  def log_analyze(log_file):
+    checked_logs = []
+    for log in log_file:
+      match = re.match(pattern, log)
+      if match:
+        log_lvl = match.group(1)
+        time = match.group(2)
+        msg = match.group(3)
+        if log != 'DEBUG':
+          checked_logs.append({'level': log_lvl, 'timestamp': time, 'message': msg})
+      else:
+        print("Log entry does not match the format expected", log)
+      return checked_logs
+
+  analysis = log_analyze(log_file)
+  
+  for logs in analysis:
+    print(analysis)
