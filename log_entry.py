cafaf9bed2272c666219991023431281d9d65b8b
Vedhanth2007 <vs396@kent.ac.uk>
2026-03-13 23:47:08 +0000

    made class called log entry

---
+++ log_entry.py
@@ -0,0 +1,8 @@
+class log_entry:
+    def __init__(self,timestamp,level,message):
+        self.timestamp = timestamp
+        self.level = level 
+        self.message = message
+    
+    def __str__(self):
+        return f"({self.timestamp},{self.level},{self.message})"
