059eed8ebd1e2abeaef83c477c5ba654d205a8c4
Vedhanth2007 <vs396@kent.ac.uk>
2026-03-21 23:16:32 +0000

    updated the regex pattern to better match the logs in the serverlog. Used an if statment to filter out any log with the level debug.

--- log_parser.py
+++ log_parser.py
@@ -9,17 +9,24 @@ class analyze_logs:
         self.outputfile = outputfile
     
     def parsing(self):
+        clean_logs = []
         with open(self.inputfile,"r") as inf, open(self.outputfile,"w") as outf:
         
             for line in inf:
-                match = re.match(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) (\w+) (\w+)', line) # can use re.search check how to use them
+                match = re.search(r'\[(\w+)\] (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}): (.*)', line) # can use re.search check how to use them
                 if match:
                     groups = match.groups()
 
-                    timestamp = groups[0]
-                    level = groups[1]
+                    timestamp = groups[1]
+                    level = groups[0]
                     message = groups[2]
 
-                    outf.write(timestamp)
-                    outf.write(level)
-                    outf.write(message)
+                    #outf.write(timestamp)
+                    #outf.write(level)
+                    #outf.write(message)
+                    #outf.write(f"{timestamp},{level},{message}\n")
+
+                    if level != 'DEBUG':
+                        outf.write(f"{timestamp},{level},{message}\n")
+                        clean_logs.append({'level': level, 'timestamp': timestamp, 'message': message})
+        return clean_logs
