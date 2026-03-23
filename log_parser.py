e9ccffb14af64f07e332927607e685d33706cc9d
Vedhanth2007 <vs396@kent.ac.uk>
2026-03-23 22:26:29 +0000

    the log parser can now communicate with the error logs class and write to a error file.txt file

--- log_parser.py
+++ log_parser.py
@@ -1,5 +1,5 @@
 import re
-from log_entry import log_entry # check if this the right way to import
+from log_entry import log_entry,Errorlogs
 
 class analyze_logs:
     
@@ -9,11 +9,11 @@ class analyze_logs:
         self.outputfile = outputfile
     
     def parsing(self):
-        clean_logs = []
-        with open(self.inputfile,"r") as inf, open(self.outputfile,"w") as outf:
+        #clean_logs = []
+        with open(self.inputfile,"r") as inf, open(self.outputfile,"w") as outf, open("Errorfile.txt", "w") as e:
         
             for line in inf:
-                match = re.search(r'\[(\w+)\] (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}): (.*)', line) # can use re.search check how to use them
+                match = re.search(r'\[(\w+)\] (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}): (.*)', line) 
                 if match:
                     groups = match.groups()
 
@@ -21,8 +21,18 @@ class analyze_logs:
                     level = groups[0]
                     message = groups[2]
 
-
+                    
+                    if level == 'ERROR':
+                        entry = Errorlogs(timestamp,level,message)
+                        e.write(str(entry) + "\n")
+                    else:
+                        entry = log_entry(timestamp,level,message)
+                    
+                    
                     if level != 'DEBUG':
-                        outf.write(f"{timestamp},{level},{message}\n")
-                        clean_logs.append({'level': level, 'timestamp': timestamp, 'message': message})
-        return clean_logs
+                        outf.write(str(entry) + "\n")
+                        #clean_logs.append(str(entry))
+                    else:
+                        pass
+
+        #return clean_logs
