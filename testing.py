0da692abd775b8ee7656fb6c98801d3ff0b020cf
Vedhanth2007 <vs396@kent.ac.uk>
2026-03-25 14:57:33 +0000

    updated testing with functions to structure the code and follow oop

--- testing.py
+++ testing.py
@@ -1,25 +1,47 @@
 # imports analyze logs from log parser
-from log_parser import analyze_logs
+from log_parser import AnalyzeLogs
 
-parser = analyze_logs("","")
+parser = AnalyzeLogs("","")
+def test_valid():
+    #test with valid log format
+    log1 = "[INFO] 2023-05-25 10:18:40: Password reset successful."
+    log2 = "[WARNING] 2023-05-25 10:18:30: High latency detected."
+    assert parser.is_valid_logs(log1) == True
+    assert parser.is_valid_logs(log2) == True
+    
 
-#test with valid log format
-log = "[INFO] 2023-05-25 10:18:40: Password reset successful."
-assert parser.is_valid_logs(log) == True
-print("Accepted the valid log,Passed valid log test")
+def test_invalid():
+    #test with invalid log format
+    log1 = "abcdefg"
+    log2 = "12345"
+    assert parser.is_valid_logs(log1) == False
+    assert parser.is_valid_logs(log2) == False
+    
 
-#test with invalid log format
-log = "abcdefg"
-assert parser.is_valid_logs(log) == False
-print("Rejected the invalid log,Passed invalid log test")
-#test to check if DEBUG logs are filtered
-level = 'DEBUG'
-tester = (level != "DEBUG")
-assert tester == False
-print("DEBUG filtered, Debug Test passed")
+def test_debug():
+    #test to check if DEBUG logs are filtered
+    level = 'DEBUG'
+    tester = (level != "DEBUG")
+    assert tester == False
+    
 
-# test to check if non DEBUG log are kept
-level = 'INFO'
-tester = (level != "DEBUG")
-assert tester == True
-print("INFO is saved, Test passed")
+def test_non_debug():
+    # test to check if non DEBUG log are kept
+    level1 = 'INFO'
+    level2 = 'ERROR'
+    tester = (level1 != "DEBUG")
+    tester = (level2 != "DEBUG")
+    assert tester == True
+    
+
+def main():
+    test_valid()
+    test_invalid()
+    test_debug()
+    test_non_debug()
+    
+    print("testing.............")
+    print("All test passed")
+
+if __name__ == "__main__":
+    main()
